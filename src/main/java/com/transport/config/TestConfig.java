061aebd61be5bf82d085bd933a4286f2059fa2ae
Breno Araujo <breno.souza.araujo@hotmail.com>
2022-07-04 14:42:30 -0300

    import.sql file created, TestConfig edited

--- src/main/java/com/transport/config/TestConfig.java
+++ src/main/java/com/transport/config/TestConfig.java
@@ -30,31 +30,33 @@ public class TestConfig implements CommandLineRunner {
 	private UserRepository userRepository;
 
 	public void run(String... args) throws Exception {
-
+	/*
 		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
-		
+
 		User u1 = new User(null, "Breno","breno.souza.araujo@hotmail.com");
 		User u2 = new User(null, "Rafaela","rafaela_cruz@gmail.com");
 		List<User> users = Arrays.asList(u1,u2);
-		
+
 		userRepository.saveAll(users);
-		
+
 
 		ShippingCompany c1 = new ShippingCompany(null, "Altura Andaimes");
 		ShippingCompany c2 = new ShippingCompany(null, "DHM transportes");
 		shippingCompanyRepository.save(c1);
 		shippingCompanyRepository.save(c2);
 
-		Transport t1 = new Transport(null, 200.00, sdf.parse("2022-01-01"), 
+		Transport t1 = new Transport(null, 200.00, sdf.parse("2022-01-01"),
 				"teste", c1,TransportType.DELIVERY);
-		Transport t2 = new Transport(null, 2000.00, sdf.parse("2022-05-01"), 
+		Transport t2 = new Transport(null, 2000.00, sdf.parse("2022-05-01"),
 				"teste", c2,TransportType.REMOVAL);
-		
+
 		t1.setUser(u1);
 		t2.setUser(u2);
-		
+
 		transportRepository.save(t1);
 		transportRepository.save(t2);
+	*/
 	}
+
 
 }
---
+++ src/main/resources/import.sql
@@ -0,0 +1,16 @@
+insert into tb_user (name, email) values ('Breno', 'breno.souza.araujo@hotmail.com');
+insert into tb_user (name, email) values ('Rafaela', 'rafaelacruz@gmail.com');
+insert into tb_user (name, email) values ('Elias', 'elias@hotmail.com');
+
+insert into tb_shipping_company (name) values ('Dhm Transportes')
+insert into tb_shipping_company (name) values ('Léo carreto')
+
+insert into tb_transport (date,observation,transport_type, value_transport, company_id, user_id) values('2022-01-01','TUBU','1','200.00','1','2')
+insert into tb_transport (date,observation,transport_type, value_transport, company_id, user_id) values('2022-01-02','BL','1','300.00','2','3')
+insert into tb_transport (date,observation,transport_type, value_transport, company_id, user_id) values('2022-03-01','BE','1','500.00','2','2')
+insert into tb_transport (date,observation,transport_type, value_transport, company_id, user_id) values('2022-04-01','MULT','1','1200.00','1','3')
+insert into tb_transport (date,observation,transport_type, value_transport, company_id, user_id) values('2022-05-01','FACH','1','150.00','1','2')
+insert into tb_transport (date,observation,transport_type, value_transport, company_id, user_id) values('2022-11-01','ELÉT.','1','300.00','1','3')
+insert into tb_transport (date,observation,transport_type, value_transport, company_id, user_id) values('2022-09-01','FACH','1','500.00','2','1')
+insert into tb_transport (date,observation,transport_type, value_transport, company_id, user_id) values('2022-11-22','BL','1','120.00','1','1')
+insert into tb_transport (date,observation,transport_type, value_transport, company_id, user_id) values('2022-01-25','BE','1','2200.00','1','1')
